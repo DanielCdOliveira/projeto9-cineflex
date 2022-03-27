7aba08263d26d2a51730a22c3f8697b5c348a191
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-03-27 13:40:11 -0300

    Reserve seats

---
+++ src/components/Session/Inputs.jsx
@@ -0,0 +1,13 @@
+function Inputs (){
+return(
+    <form className="inputs">
+        <label>Nome do comprador:</label>
+        <input type="text" name="" id=""  placeholder="Digite seu nome..."/>
+        <label>CPF do comprador:</label>
+        <input type="number" placeholder="Digite seu CPF..." />
+        <button type="submit">Reservar assento(s)</button>
+    </form>
+)
+}
+
+export default Inputs;
--- src/components/Session/index.jsx
+++ src/components/Session/index.jsx
@@ -3,26 +3,65 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 import Description from "./Description";
+// import Inputs from "./Inputs";
 
 function Session() {
   const { idSessao } = useParams();
-  console.log(idSessao);
   const [session, setSession] = useState([]);
-
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [seatsArray, setSeatsArray] = useState([]);
+  const [selected, setSelected] = useState()
   useEffect(() => {
     const promise = axios.get(
       `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`
     );
 
     promise.then((response) => {
+      console.log("foiii");
       setSession(response.data);
     });
     promise.catch(console.log("deu merda"));
   }, []);
 
-  console.log(session);
-  console.log(session.length);
+  function selectSeat(seat) {
+    setSelected("selected")
 
+    seat = parseInt(seat);
+
+    let index = seatsArray.indexOf(seat);
+    if (index > -1) {
+      setSeatsArray(
+        seatsArray.filter((item) => {
+          return item != seat;
+        })
+      );
+    } else {
+      setSeatsArray([...seatsArray, seat]);
+    }
+  }
+
+  console.log(seatsArray);
+  function reserveSeats(e) {
+    e.preventDefault();
+
+    const seats = {
+      ids: seatsArray,
+      name: name,
+      cpf: cpf,
+    };
+    console.log(seats);
+    const promise = axios.post(
+      "https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many",
+      seats
+    );
+
+    promise.then((response) => {
+      console.log(response);
+    });
+    promise.catch(console.log("deu xabu"));
+  }
+  console.log(session);
   if (Object.keys(session).length !== 0) {
     return (
       <main>
@@ -30,21 +69,46 @@ function Session() {
         <ul className="seats">
           {session.seats.map((seat) => {
             return (
-              <li className="seat" key={seat.id}>
+              <li
+                onClick={() => {
+                  selectSeat(seat.name);
+                }}
+                className={seat.isAvailable ? `seat` : 'seat occupied'}
+                key={seat.id}
+              >
                 {seat.name}
               </li>
             );
           })}
         </ul>
-       <Description />
+        <Description />
+        <form onSubmit={reserveSeats} className="inputs">
+          <label>Nome do comprador:</label>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Digite seu nome..."
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>CPF do comprador:</label>
+          <input
+            type="number"
+            maxLength={11}
+            placeholder="Digite seu CPF..."
+            required
+            value={cpf}
+            onChange={(e) => setCpf(e.target.value)}
+          />
+          <button type="submit">Reservar assento(s)</button>
+        </form>
       </main>
     );
   } else {
     return <></>;
   }
 }
-
-
-
 
 export default Session;
